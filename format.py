0e7c647970925e8f4fe965a655c7f6267deccd45
Gaëtan <73687455+plapla-1357@users.noreply.github.com>
2025-03-29 10:12:10 +0100

    ajout de donné + amelioration de l'affichage

---
+++ format.py
@@ -0,0 +1,20 @@
+
+
+def format(name):
+    with open(name, "r+") as f:
+        filedata = f.read()
+    print(filedata)
+    filedata = filedata.replace(",", ".") # remplace les , en .
+    filedata = filedata.replace(";", ",") # remplace les ; en ,
+    lines = filedata.split("\n")
+    lines = lines[1:] # retirer la première ligne
+    lines[0] = lines[0][:-1] # retire la dernière virgule des entètes
+    filedata = "\n".join(lines)
+    
+    with open(name, 'w') as file:
+        file.write(filedata)
+        
+if __name__ == "__main__":
+    name = input("fichier a formater: ")
+    format(name)
+        
